0c087801605ed74f6455da5f3e705b66c9ee3929
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-30 01:21:53 +0600

    written code for taking 'positional-argument' and restructured the code to make it look nicer

--- 01_bestdayever.sh
+++ 01_bestdayever.sh
@@ -1,34 +1,46 @@
 #!/usr/bin/bash
 
 ## step 1:Print without any variable
-#echo "Good Morning Chuck!"
-#sleep 1
-#echo "You're looking good today Chuck!"
-#sleep 1
-#echo "You have the best beard I've ever seen Chuck!!"
+# echo "Good Morning Chuck!"
+# sleep 1
+# echo "You're looking good today Chuck!"
+# sleep 1
+# echo "You have the best beard I've ever seen Chuck!!"
 
 
 ## step 2:Print with a variable [assigned value]
 
 # don't keep any space between equal sign and variable value
-#name="Chuck"
-#echo "Good Morning $name!"
-#sleep 1
-#echo "You're looking good today $name!"
-#sleep 1
-#echo "You have the best beard I've ever seen $name!!"
+# name="Chuck"
+# echo "Good Morning $name!"
+# sleep 1
+# echo "You're looking good today $name!"
+# sleep 1
+# echo "You have the best beard I've ever seen $name!!"
 
 
-## step 2:Print with a variable [scanned value]
+## step 3:Print with a variable [scanned value]
+
+# echo "Enter a name: "
+# read name
+# echo "Good Morning $name!"
+# sleep 1
+# echo "You're looking good today $name!"
+# sleep 1
+# echo "You have the best beard I've ever seen $name!!"
+
+
+## step 4:Taking Parameter and Print them
 
 # don't keep any space between equal sign and variable value
-echo "Enter a name: "
-read name
+# structure: <file-name>.sh <space>  <1st-param> <space> <2nd-param> ....
+# example: "./01_bestdayever.sh emdad beard"
+name=$1 #1st positional param
+object_to_complement=$2 #2nd positional param
+
 echo "Good Morning $name!"
 sleep 1
-
 echo "You're looking good today $name!"
 sleep 1
-
-echo "You have the best beard I've ever seen $name!!"
+echo "You have the best $object_to_complement I've ever seen $name!!"
 
