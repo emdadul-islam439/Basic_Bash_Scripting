a5362fcf6b7fd89a84edb41aa3c5137c34107793
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-30 00:46:45 +0600

    initial commit with lot of history=> created file with 'nano 01_bestdayever.sh', written code-> saved, made it executable with 'chmod +x 01_bestdayever.sh', executed with './01_bestdayever.sh'

---
+++ 01_bestdayever.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+## step 1:Print without any variable
+#echo "Good Morning Chuck!"
+#sleep 1
+#echo "You're looking good today Chuck!"
+#sleep 1
+#echo "You have the best beard I've ever seen Chuck!!"
+
+
+## step 2:Print with a variable [assigned value]
+
+# don't keep any space between equal sign and variable value
+#name="Chuck"
+#echo "Good Morning $name!"
+#sleep 1
+#echo "You're looking good today $name!"
+#sleep 1
+#echo "You have the best beard I've ever seen $name!!"
+
+
+## step 2:Print with a variable [scanned value]
+
+# don't keep any space between equal sign and variable value
+echo "Enter a name: "
+read name
+echo "Good Morning $name!"
+sleep 1
+
+echo "You're looking good today $name!"
+sleep 1
+
+echo "You have the best beard I've ever seen $name!!"
+
