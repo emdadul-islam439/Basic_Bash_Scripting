5acf7eb4ae5be9492db4d630ebf6329e519fa2d4
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-30 02:02:12 +0600

    created a new file to working and documenting of different bash terminal command, added documentation about 'ls' command

---
+++ 02_aboutbashterminal.sh
@@ -0,0 +1,5 @@
+## 1: The 'ls' command
+print_working_directory=$(pwd)
+list_of_directories_and_files=$(ls)
+echo "these are the directories and files: $list_of_directories_and_files"
+echo "this is your current working directory: $print_working_directory"
