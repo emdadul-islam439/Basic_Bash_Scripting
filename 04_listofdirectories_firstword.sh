1688088051d9233ddc69186e50df13ae795ab806
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-31 00:46:16 +0600

    tried to make a list, but the directory list become a whole string

--- 04_listofdirectories_firstword.sh
+++ 04_listofdirectories_firstword.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-echo "hello"
+myDir='/home/emdad439/Developing'
+
+## The followin line has two commands
+# first : go to 'myDir' location
+# second: list all directories ends with '/' char in 'myDir' location
+#	: and print only the portions before first space of each one
+first_word_of_directories=$(cd ${myDir} && /usr/bin/ls -d */ | cut -d " " -f 1)
+echo first directory = ${first_word_of_directories[0]}
+echo second_directory = ${first_word_of_directories[1]}
+
+
+## INPUT:
+# ./04_listofdirectories_firstword.sh
+
+## OUTPUT:
+# first directory = Bash_Scripting/ Django_Journey/
+# second_directory =
