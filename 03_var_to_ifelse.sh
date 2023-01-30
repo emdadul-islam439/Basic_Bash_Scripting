61881580f6ad465def972bbad86f5b104c0b99c6
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-31 00:03:22 +0600

    WORKEDgit status! at last I've successfully coded the if related blocks and got the desired output!

--- 03_var_to_ifelse.sh
+++ 03_var_to_ifelse.sh
@@ -13,17 +13,19 @@ echo arr[0]=${arr[0]}, arr[1]=${arr[1]}
 echo choice=$choice
 
 if [ $choice == ${arr[0]} ]; then
-  echo "${arr[0]}"
-elif [ "$choice"=="$arr[1]" ]; then
-  echo $arr[1]
-elif [ "$choice"=="$arr[2]" ]; then
-  echo $arr[2]
+  echo ${arr[0]}
+elif [ $choice == ${arr[1]} ]; then
+  echo ${arr[1]}
+elif [ $choice == ${arr[2]} ]; then
+  echo ${arr[2]}
 fi
 
 echo Last Line!
 
+## INPUT:
+# ./03_var_to_ifelse.sh option
+
 ## OUTPUT:
 # arr[0]=option1, arr[1]=option2
 # choice=option
-# option1[1]
 # Last Line!
