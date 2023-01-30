d32bad70a4814cf50861cf046a6c02969d7f2358
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-30 22:53:27 +0600

    SEE-IF-IT-WORKS: tired to take 'positional-field-parameter' and match it with different predefined strings with if-else

--- 03_var_to_ifelse.sh
+++ 03_var_to_ifelse.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/bash 
 # we can use this also-> "#!/usr/bin/env bash"
 
-firefox
-sleep 3
-killall firefox
+arr=(
+option1
+option2
+option3
+)
+
+choice=$1
+
+if[$choice==$arr[0]]; then
+  echo arr[0]
+elif[$choice==$arr[1]]; then
+  echo arr[1]
+elif[$choice==$arr[2]]; then
+  echo arr[2]
+else; then
+  echo Nothing Found!
+fi
