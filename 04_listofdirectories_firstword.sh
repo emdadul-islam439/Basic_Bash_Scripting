4987a7591c5dc1c80926dab72a82046625a58cd8
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-31 00:49:26 +0600

    SUCCESSgit status took another variable and converted the string into array of strings.

--- 04_listofdirectories_firstword.sh
+++ 04_listofdirectories_firstword.sh
@@ -7,13 +7,14 @@ myDir='/home/emdad439/Developing'
 # second: list all directories ends with '/' char in 'myDir' location
 #	: and print only the portions before first space of each one
 first_word_of_directories=$(cd ${myDir} && /usr/bin/ls -d */ | cut -d " " -f 1)
-echo first directory = ${first_word_of_directories[0]}
-echo second_directory = ${first_word_of_directories[1]}
+arr=($first_word_of_directories)
+echo first directory = ${arr[0]}
+echo second_directory = ${arr[1]}
 
 
 ## INPUT:
 # ./04_listofdirectories_firstword.sh
 
 ## OUTPUT:
-# first directory = Bash_Scripting/ Django_Journey/
-# second_directory =
+# first directory = Bash_Scripting/
+# second_directory = Django_Journey/
