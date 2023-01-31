d55aa3691b8d06cd0d460fe1f24935f0a4a7d61b
Emdadul_Islam <emdadulislam439@gmail.com>
2023-02-01 02:31:06 +0600

    created 00_experiments.sh file to documenting different experiments

---
+++ 00_experiments.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+## Start Executing this script file with "LESS" command
+less #Failed!! Output->"Missing filename ("less --help" for help)" 
+
+
+### experimenting to execute another shell script from this file
+./05_makeMultipleFileAndRemove.sh  #successfully executed!
