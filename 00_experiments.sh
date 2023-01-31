39cc1333afed8767821f0e7c2ab08d0430692d61
Emdadul_Islam <emdadulislam439@gmail.com>
2023-02-01 02:43:15 +0600

    added a curious-question related to less command execution from a script

--- 00_experiments.sh
+++ 00_experiments.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 
-## Start Executing this script file with "LESS" command
-less #Failed!! Output->"Missing filename ("less --help" for help)" 
+### Start Executing this script file with "LESS" command
+less	 #Failed!! Output->"Missing filename ("less --help" for help)" 
+# Question: I can open execute this file with "less"
+# 	    by writing "./00_experiments.sh | less"
+#	    This is from command line. But, how can I achieve that
+#	    from within this script???
+
 
 
 ### experimenting to execute another shell script from this file
