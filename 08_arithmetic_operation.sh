5c50e0ccad7d7c4697c04d327ebcbc44c5b4e11e
Emdadul_Islam <emdadulislam439@gmail.com>
2023-02-01 15:22:05 +0600

    created new file to document arithmetic operations

---
+++ 08_arithmetic_operation.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+######### BASIC INFO #########
+# ::Basic writing form::
+# arithmetic expression written in this way -> (( ))
+# Example: (( 8 + 5 )), (( 5**3 )) [exponent], (( 2*3 ))
+# not-arithmetic: 8 + 5 [ without parenthesis,  "85" -> string]
+# 
+# ::Assigning to a variable::
+# <variable-name>=$(( <arithmetic-expression> ))
+# Example: addition=$(( 85 + 92 )), exponentiation=$(( 6**9 ))
+# ###########################
+
+
