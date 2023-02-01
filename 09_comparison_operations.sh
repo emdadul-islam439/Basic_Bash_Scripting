758b2dbfe5fd88ac3562eeb9625f6f5a22575dac
Emdadul_Islam <emdadulislam439@gmail.com>
2023-02-01 18:26:49 +0600

    added some more info and examples

--- 09_comparison_operations.sh
+++ 09_comparison_operations.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 ########### BASIC COMPARISON ############
-# syntex	: [[ <var/num/str> <comparison-sign> <var/num/str> ]]]
+# syntex	: [[ <var_with_dollar/num/str> <comparison-sign> <var_with_dollar/num/str> ]]]
 # Exmaple	: str-comparison-> [[ $a < $b ]], [[ 2 >= 3 ]] { both a,b and 2,3 are string }
 #	 	: arith-comparison-> [[ $a -lt $b ]], [[ 2 -ge 3 ]] { both a,b and 2,3 are int }
 #
 # ouput		: 0-> for 'true', 1-> for 'false'
 # other-info	: str-signs==>   ' < ', ' > ', ' <= ', ' >= ', ' == ', ' != ' 
 # 		: arith-signs==> '-lt', '-gt', ' -le', ' -ge', ' -eq', ' -ne'
-########################################
+#		: check null-notNull==> null:'[[ -z $a ]]' not-null:'[[ -n $a ]]'
+#########################################
 
 
 ### Basic 'str' comparisons
