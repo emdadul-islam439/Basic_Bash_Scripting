d4d7a5fad5799a34630143d48ca19ad103654240
Emdadul_Islam <emdadulislam439@gmail.com>
2023-02-01 17:04:58 +0600

    created file for documenting comparison related operations

---
+++ 09_comparison_operations.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+########### BASIC COMPARISON ############
+# syntex	: [[ <var/num/str> <comparison-sign> <var/num/str> ]]]
+# Exmaple	: str-comparison-> [[ $a < $b ]], [[ 2 >= 3 ]] { both a,b and 2,3 are string }
+#	 	: arith-comparison-> [[ $a -lt $b ]], [[ 2 -ge 3 ]] { both a,b and 2,3 are int }
+#
+# ouput		: 0-> for 'true', 1-> for 'false'
+########################################
