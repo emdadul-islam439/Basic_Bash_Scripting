6c30d18b65b013ad5968ea957310f0296475b06e
Emdadul_Islam <emdadulislam439@gmail.com>
2023-02-01 23:50:17 +0600

    created different styled-text

--- 11_echo_colored_text.sh
+++ 11_echo_colored_text.sh
@@ -14,7 +14,11 @@
 #		:
 # Color-code	: Color(front,back)=> Black(30,40), Red(31,41), Green(32,42), Yellow(33,43)
 #		:		      Blue(34,44), Magenta(35,45), Cyan(36,46), White(37,47)
+#		:
+# Style-code	: Style-name(code)=> No-style(0), Bold(1), Low-intensity(2), Underline(4), 
+#		: 		     Blinking(5),Reverse(7), Invinsible(8)
 ###############################################
+
 
 ### a simpled styled escaped-string
 sleep 1
@@ -49,5 +53,39 @@ sleep 1
 echo "echo -e '\033[31;47mRed on White\033[0m' -->"
 sleep 1
 echo -e '\033[31;47mRed on White\033[0m'
+sleep 1
+echo
+
+
+### Different Colored-texts with Styles
+sleep 1
+echo
+sleep 1
+echo
+sleep 1
+echo "echo -e '\033[1;30;41mStyle:BOLD |  Black on Red\033[0m' -->"
+sleep 1
+echo -e '\033[1;30;41mStyle:BOLD |  Black on Red\033[0m'
+sleep 1
+echo
+
+sleep 1
+echo "echo -e '\033[4;37;40mStyle:UNDERLINED |  White on Black\033[0m'-->"
+sleep 1
+echo -e '\033[4;37;40mStyle:UNDERLINED |  White on Black\033[0m'
+sleep 1
+echo
+
+sleep 1
+echo "echo -e '\033[7;32;40mStyle:REVERSE | Green on Black\033[0m'-->"
+sleep 1
+echo -e '\033[7;32;40mStyle:REVERSE | Green on Black\033[0m'
+sleep 1
+echo
+
+sleep 1
+echo "echo -e '\033[5;31;47mStyle:BLINKING | Red on White\033[0m' -->"
+sleep 1
+echo -e '\033[5;31;47mStyle:BLINKING | Red on White\033[0m'
 sleep 1
 echo
